125536ea9002a0c87241b2357b07154f4f858bfc
Alexander Pustovalov <apustovalov@gmail.com>
2019-11-29 23:52:05 +0200

    Next phase

--- src/usr/in-page-components/layouts/Box.comp.js
+++ src/usr/in-page-components/layouts/Box.comp.js
@@ -2,41 +2,42 @@ import pickBy from 'lodash/pickBy';
 import isNumber from 'lodash/isNumber';
 import React from 'react';
 import BoxMUI from '@material-ui/core/Box';
-import { useTheme } from '@material-ui/core/styles';
+import { withTheme } from '@material-ui/core/styles';
 import findColor from '../../common-props/utils/colorMap';
 import { BoxTypes } from './Box.props';
 
-const Box = (props) => {
-  const theme = useTheme();
-  const { content, borders, palette } = props;
-  let properties = {};
-  if (borders) {
-    const { border, borderTop, borderRight, borderBottom, borderLeft, borderColor } = borders;
-    properties = {
-      ...properties,
-      ...{...pickBy({border, borderTop, borderRight, borderBottom, borderLeft}, isNumber)}
-    };
-    if (borderColor) {
-      const { colorHue, colorShade } = borderColor;
-      properties.borderColor = findColor(colorHue, colorShade, theme);
+class Box extends React.Component {
+  render() {
+    const { content, borders, palette, theme } = this.props;
+    let properties = {};
+    if (borders) {
+      const { border, borderTop, borderRight, borderBottom, borderLeft, borderColor } = borders;
+      properties = {
+        ...properties,
+        ...pickBy({ border, borderTop, borderRight, borderBottom, borderLeft }, isNumber)
+      };
+      if (borderColor) {
+        const { colorHue, colorShade } = borderColor;
+        properties.borderColor = findColor(colorHue, colorShade, theme);
+      }
     }
+    const { color, backgroundColor } = palette;
+    if (color) {
+      const { colorHue, colorShade } = color;
+      properties.color = findColor(colorHue, colorShade, theme);
+    }
+    if (backgroundColor) {
+      const { colorHue, colorShade } = backgroundColor;
+      properties.bgcolor = findColor(colorHue, colorShade, theme);
+    }
+    console.info('properties: ', properties);
+    return (
+      <BoxMUI {...properties}>
+        {content}
+      </BoxMUI>
+    );
   }
-  const { color, backgroundColor } = palette;
-  if (color) {
-    const { colorHue, colorShade } = color;
-    properties.color = findColor(colorHue, colorShade, theme);
-  }
-  if (backgroundColor) {
-    const { colorHue, colorShade } = backgroundColor;
-    properties.bgcolor = findColor(colorHue, colorShade, theme);
-  }
-  console.info('properties: ', properties);
-  return (
-    <BoxMUI {...properties}>
-      {content}
-    </BoxMUI>
-  );
-};
+}
 
 Box.propTypes = BoxTypes;
 
@@ -58,4 +59,4 @@ Box.defaultProps = {
   content: [<span/>],
 };
 
-export default Box;
+export default withTheme(Box);
