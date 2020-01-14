0e3977c55d79fb550cbe5c0684713d6cbcf10de7
Alexander Pustovalov <apustovalov@gmail.com>
2020-01-14 23:02:50 +0200

    Next phase

--- src/app/indices-prod/userFunctions.js
+++ src/app/indices-prod/userFunctions.js
@@ -1,2 +1,2 @@
 
-export default {};
+export default {'usr':{'page':{'PageFrameWithDrawer':require('usr/page/PageFrameWithDrawer.funcs'),'PageNavigation':require('usr/page/PageNavigation.funcs'),},},};
